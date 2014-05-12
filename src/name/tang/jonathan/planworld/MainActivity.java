995f6b1fc4f7f88d9d5433ad2cac617e1f771c3d
Jonathan Tang <jonathan.d.tang@gmail.com>
2014-05-12 14:25:19 -0700

    Added simple communication between WebView and native app.

--- src/name/tang/jonathan/planworld/MainActivity.java
+++ src/name/tang/jonathan/planworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
@@ -11,21 +12,28 @@ import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
 
+	private static final String PLANWATCH_EXTRACTION_JS = "javascript:"
+			+ "var names = [];" 
+			+ "var links = document.querySelectorAll('a.planwatch');"
+			+ "for (var i = 0, link; link = links[i++];) {"
+				+ "names.push(link.innerText);"
+			+ "}"
+			+ "alert(names);";
+	
+	private ListView planWatch;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		WebView planWorldWeb = (WebView) findViewById(R.id.webview);
-		planWorldWeb.setWebViewClient(new WebViewClient() {
-			@Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				return true;
-			}
-		});
+		planWorldWeb.setWebViewClient(new PlanScraperClient());
 		planWorldWeb.getSettings().setJavaScriptEnabled(true);
+		planWorldWeb.addJavascriptInterface(new JSPlanDroid(), "plandroid");
 		planWorldWeb.loadUrl("https://neon.note.amherst.edu/planworld");
+		planWorldWeb.loadUrl("javascript:plandroid.execute()");
 		
-		ListView planWatch = (ListView) findViewById(R.id.planwatch);
+		planWatch = (ListView) findViewById(R.id.planwatch);
 		planWatch.setAdapter(new ArrayAdapter<String>(
 				this, android.R.layout.simple_list_item_1, new String[] {
 						"person1", "person2", "person3", "person4", "person5"
@@ -50,5 +58,26 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class PlanScraperClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			return false;
+		}
+	}
+	
+	private class JSPlanDroid implements Runnable {
+		@JavascriptInterface
+		public void execute() {
+			planWatch.post(this);
+		}
+		
+		// Executes in UI thread, called back via planWatch.post();
+		public void run() {
+			planWatch.setAdapter(new ArrayAdapter(
+					MainActivity.this, android.R.layout.simple_list_item_1,
+					new String[] { "JS Invoked!" }));
+		}
 	}
 }
