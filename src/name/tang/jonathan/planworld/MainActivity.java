1c76ffe05fc045b5ddbb0b1fee5c2a72c1f3ee93
Jonathan Tang <jonathan.d.tang@gmail.com>
2014-05-13 00:37:53 -0700

    Refactored WebView code into a fragment.

--- src/name/tang/jonathan/planworld/MainActivity.java
+++ src/name/tang/jonathan/planworld/MainActivity.java
@@ -1,45 +1,33 @@
 package name.tang.jonathan.planworld;
 
-import java.util.List;
-
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+		implements PlanScraperFragment.ScrapeCompletedListener {
 
-	private static final String PLANWATCH_EXTRACTION_JS = "javascript:"
-			+ "var names = [];" 
-			+ "var links = document.querySelectorAll('a.planwatch');"
-			+ "for (var i = 0, link; link = links[i++];) {"
-				+ "names.push(link.innerText);"
-			+ "}"
-			+ "plandroid.recordPlanWatch(names.join(','));";
-	
 	private ListView planWatch;
+	private PlanScraperFragment scraper;
 	private PlandroidDatabase dbHelper;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		WebView planWorldWeb = (WebView) findViewById(R.id.webview);
-		planWorldWeb.setWebViewClient(new PlanScraperClient());
-		planWorldWeb.getSettings().setJavaScriptEnabled(true);
-		planWorldWeb.addJavascriptInterface(new JSPlanDroid(), "plandroid");
-		planWorldWeb.loadUrl("https://neon.note.amherst.edu/planworld");
 		
 		planWatch = (ListView) findViewById(R.id.planwatch);
 		planWatch.setAdapter(new ArrayAdapter<String>(
 				this, android.R.layout.simple_list_item_1, new String[] {
 						"person1", "person2", "person3", "person4", "person5"
 				}));
+		
+		scraper = (PlanScraperFragment)
+				getFragmentManager().findFragmentById(R.id.scraper);
+		scraper.setListener(this);
 		
 		dbHelper = new PlandroidDatabase(this);
 	}
@@ -64,34 +52,10 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	private class PlanScraperClient extends WebViewClient {
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			return false;
-		}
-		
-		@Override
-		public void onPageFinished(WebView view, String url) {
-			if (url.indexOf("planworld") != -1) {
-				view.loadUrl(PLANWATCH_EXTRACTION_JS);
-			}
-		}
-	}
-	
-	private class JSPlanDroid implements Runnable {
-		private String[] usernames;
-		
-		@JavascriptInterface
-		public void recordPlanWatch(String usernames) {
-			this.usernames = usernames.split(",");
-			planWatch.post(this);
-		}
-		
-		// Executes in UI thread, called back via planWatch.post();
-		public void run() {
-			planWatch.setAdapter(new ArrayAdapter(
-					MainActivity.this, android.R.layout.simple_list_item_1,
-					usernames));
-		}
+	@Override
+	public void onScrapeCompleted() {
+		planWatch.setAdapter(new ArrayAdapter(
+				MainActivity.this, android.R.layout.simple_list_item_1,
+				scraper.getUsernames()));
 	}
 }
---
+++ src/name/tang/jonathan/planworld/PlanScraperFragment.java
@@ -0,0 +1,92 @@
+package name.tang.jonathan.planworld;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * 
+ */
+public class PlanScraperFragment extends Fragment {
+
+	private static final String PLANWATCH_EXTRACTION_JS = "javascript:"
+			+ "var names = [];" 
+			+ "var links = document.querySelectorAll('a.planwatch');"
+			+ "for (var i = 0, link; link = links[i++];) {"
+				+ "names.push(link.innerText);"
+			+ "}"
+			+ "plandroid.recordPlanWatch(names.join(','));";
+	
+	private WebView browser;
+	private ScrapeCompletedListener listener;
+	private String[] usernames;
+	
+	public PlanScraperFragment() {
+		usernames = new String[0];
+		listener = new ScrapeCompletedListener() {
+			@Override public void onScrapeCompleted() {
+				// Null object
+			}
+		};
+	}
+
+	public void setListener(ScrapeCompletedListener listener) {
+		this.listener = listener;
+	}
+	
+	public String[] getUsernames() {
+		return this.usernames;
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_plan_scraper, container,
+				false);
+		
+		browser = (WebView) view.findViewById(R.id.webview);
+		browser.setWebViewClient(new PlanScraperClient());
+		browser.getSettings().setJavaScriptEnabled(true);
+		browser.addJavascriptInterface(new JSPlanDroid(), "plandroid");
+		browser.loadUrl("https://neon.note.amherst.edu/planworld");
+		return view;
+	}
+
+	public static interface ScrapeCompletedListener {
+		public void onScrapeCompleted();
+	}
+	
+	private static class PlanScraperClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			return false;
+		}
+		
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			if (url.indexOf("planworld") != -1) {
+				view.loadUrl(PLANWATCH_EXTRACTION_JS);
+			}
+		}
+	}
+	
+	private class JSPlanDroid implements Runnable {
+		@JavascriptInterface
+		public void recordPlanWatch(String usernames) {
+			PlanScraperFragment.this.usernames = usernames.split(",");
+			browser.post(this);
+		}
+		
+		// Executes in UI thread, called back via planWatch.post();
+		public void run() {
+			listener.onScrapeCompleted();
+		}
+	}
+}
